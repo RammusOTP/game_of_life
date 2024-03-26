365fb937adf3ac3559c591de4fe9ad8d60222824
Emmanuel Phillips <emmanuel.j.phillips@pwc.com>
2024-03-26 09:11:08 +0000

    Initial commit

---
+++ src/funcs.py
@@ -0,0 +1,26 @@
+import colours
+import numpy as np
+import pygame
+
+def update(screen, cells, size, with_progress=False):
+    updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
+
+    for row, col in np.ndindex(cells.shape):
+        alive = np.sum(cells[row - 1: row + 2, col - 1: col + 2]) - cells[row, col]
+        colour = colours.BG if cells[row, col] > 0 else colours.ALIVE_NEXT
+        if cells[row, col] == 1:
+            if alive < 2 or alive > 3:
+                if with_progress:
+                    colour = colours.DIE_NEXT
+            elif 2 <= alive <= 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    colour = colours.ALIVE_NEXT
+        else:
+            if alive == 3:
+                updated_cells[row, col] = 1
+                if with_progress:
+                    colour = colours.ALIVE_NEXT
+
+        pygame.draw.rect(screen, colour, (col * size, row * size, size - 1, size - 1))
+    return updated_cells
