b047991049f3131bfe90ba85ab93c62d2236b722
Emmanuel Phillips <emmanuel.j.phillips@pwc.com>
2024-03-27 09:13:19 +0000

    Improved comments

--- src/test_runner.py
+++ src/test_runner.py
@@ -1,7 +1,12 @@
 from test_funcs import *
-
+from colours import RED, GREEN, RESET
 
 def run_tests():
+    """
+    Iterates through the tests made in test_funcs.py and prints a nicer error message if failed.
+
+    :return: PASSED or FAILED with error message
+    """
     test_functions = [test_update_without_progress, test_update_with_progress]
 
     passed_tests = 0
@@ -10,10 +15,10 @@ def run_tests():
     for test_func in test_functions:
         try:
             test_func()
-            print(f"\033[92m[Passed]\033[0m {test_func.__name__}")
+            print(f"{GREEN}[PASSED]{test_func.__name__}{RESET}")
             passed_tests += 1
         except AssertionError as e:
-            print(f"\033[91m[Failed]\033[0m {test_func.__name__}: {e}")
+            print(f"{RED}[FAILED]{test_func.__name__}: {e}{RESET}")
 
     print(f"\n{passed_tests}/{total_tests} tests passed.")
 
